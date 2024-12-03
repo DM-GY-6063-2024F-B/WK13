7e6b8102fb213424dbfc394bcbc17aeb20b6748c
Thiago Hersan <thiago.hersan+github@gmail.com>
2024-12-03 01:44:16 -0500

    code from class

--- 01_Serial/sketch.js
+++ 01_Serial/sketch.js
@@ -4,13 +4,6 @@ let connectButton;
 
 let cBackgroundColor;
 
-function receiveSerial() {
-  let line = mSerial.readUntil("\n");
-  print(line);
-
-  // TODO: assign to color
-}
-
 function connectToSerial() {
   if (!mSerial.opened()) {
     mSerial.open(9600);
@@ -21,19 +14,21 @@ function connectToSerial() {
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
-  cBackgroundColor = 0;
-
   mSerial = createSerial();
 
   connectButton = createButton("Connect To Serial");
   connectButton.position(width / 2, height / 2);
   connectButton.mousePressed(connectToSerial);
+
+  cBackgroundColor = 0;
 }
 
 function draw() {
   background(cBackgroundColor);
 
   if (mSerial.opened() && mSerial.availableBytes() > 0) {
-    receiveSerial();
+    let line = mSerial.readUntil("\n");
+    let sensorVal = int(line);
+    cBackgroundColor = map(sensorVal, 0, 2000, 0, 255, true);
   }
 }
